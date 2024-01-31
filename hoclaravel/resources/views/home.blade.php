df84c9601a3c85d2c8c4c412c5bf85047497d13b
hue <hue.ho25@student.passerellesnumeriques.org>
2024-01-31 14:02:26 +0700

    bai 3

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,1 +1,8 @@
 <h1 style="text-align: center;">Học lập trình Laravel tại Unicode </h1>
+<?php 
+if (env('APP_ENV')=='production'){
+    //Call api live;
+}else{
+    //Call api sandbox
+    echo 'Call api sandbox';
+}
