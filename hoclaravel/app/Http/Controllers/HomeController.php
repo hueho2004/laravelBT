01359a5e249ce4e4a61ecbe72a9a95206cb8145e
hue <hue.ho25@student.passerellesnumeriques.org>
2024-01-31 14:54:14 +0700

    bai 5

--- hoclaravel/app/Http/Controllers/HomeController.php
+++ hoclaravel/app/Http/Controllers/HomeController.php
@@ -6,8 +6,16 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    // action index 
     public function index(){
-        
+        return 'Home';
+    }
+
+    public function getNews(){
+        return "Danh sách tin tức";
+    }
+
+    public function getCategory($id){
+        return "Danh sách category ". $id;
     }
 }
--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,8 +1,18 @@
-<h1 style="text-align: center;">Học lập trình Laravel tại Unicode </h1>
-<?php 
-if (env('APP_ENV')=='production'){
-    //Call api live;
-}else{
-    //Call api sandbox
-    echo 'Call api sandbox';
+<h1>Welcome to Learning_Laravel at My Website</h1>
+<a href="<?php echo route('admin.show-form') ?>">Show form</a>
+<a href="<?php echo route('admin.products.add') ?>">Thêm sản phẩm</a>
+<a href="<?php echo route('admin.laravel', ['id'=>1, 'slug'=>'tin-tuc-the-gioi']) ?>">tin tức</a>
+<?php
+// Output the value of APP_ENV using env() function
+echo env('APP_ENV');
+
+// Output the value of APP_ENV using config() function
+echo config('app.env');
+
+// Check if the environment is production
+if (env('APP_ENV') == 'production') {
+    echo 'call api live';
+} else {
+    echo 'call api sandbox';
 }
+?>
