807c481ffebd3d0154e8083e9d6cec7bf6d21c5d
hue <hue.ho25@student.passerellesnumeriques.org>
2024-02-19 14:12:04 +0700

    bai 8

--- hoclaravel/app/Http/Controllers/HomeController.php
+++ hoclaravel/app/Http/Controllers/HomeController.php
@@ -3,19 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class HomeController extends Controller
 {
-    // action index 
-    public function index(){
-        return 'Home';
+
+    public function index()
+    {
+        $title = "Học lập trình web tại Diepthao.com";
+        $content = "Học Laravel tại Diepthao.com";
+        /*
+        $dataView = [
+            'title' => $title,
+            'content' => $content
+        ];
+    -------
+        $contentView = view('home');
+        $contentView= $contentView->render();// Khi mong muốn xử lý dữ liệu thô
+        echo $contentView;
+          return View::make('home', compact('title', 'content'));
+        return view('home')->with(['title'=>$title, 'content'=>$content]);
+*/
+        return view('home', compact('title', 'content'));
+
     }
 
-    public function getNews(){
+
+    public function getNews()
+    {
         return "Danh sách tin tức";
     }
 
-    public function getCategory($id){
-        return "Danh sách category ". $id;
+    public function getCategory($id)
+    {
+        return "Danh sách category " . $id;
+    }
+
+     public function getProductDetail($id)
+    {
+     //  return $id;
+      // return "Danh sách category " . $id;
+      //  return view('clients/products/detail', compact('id'));
+        return view('clients.products.detail', compact('id'));
     }
 }
--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,18 +1,22 @@
-<h1>Welcome to Learning_Laravel at My Website</h1>
-<a href="<?php echo route('admin.show-form') ?>">Show form</a>
-<a href="<?php echo route('admin.products.add') ?>">Thêm sản phẩm</a>
-<a href="<?php echo route('admin.laravel', ['id'=>1, 'slug'=>'tin-tuc-the-gioi']) ?>">tin tức</a>
-<?php
-// Output the value of APP_ENV using env() function
-echo env('APP_ENV');
-
-// Output the value of APP_ENV using config() function
-echo config('app.env');
-
-// Check if the environment is production
-if (env('APP_ENV') == 'production') {
-    echo 'call api live';
-} else {
-    echo 'call api sandbox';
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel-Học Lập trình căn bản </title>
+</head>
+<body>
+    <header>
+    <h1>Laravel-Header</h1>
+    <h2><?php echo $title ?></h2>
+    </header>
+    <main>
+        <h1>Content-Laravel</h1>
+          // <h2><?php echo $content ?></h2>
+          <h2>{$content} </h2>
+    </main>
+    <footer>
+        <h1>Footer-Laravel</h1>
+    </footer>
+</body>
+</html>
