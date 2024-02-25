bae393bd75b173ef0dbea547afe312fef95655f4
hue <hue.ho25@student.passerellesnumeriques.org>
2024-02-25 20:57:34 +0700

    bai 12

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -8,6 +8,7 @@
 </head>
 
 <body>
+
     <h1>Trang chủ Laravel</h1>
     <h2>{{$welcome}}</h2>
     <h2>{{!empty(request()->keyword) ? request()->keyword : "Không có gì"}}</h2>
@@ -15,31 +16,11 @@
         {{!! !empty($content) ? $content : false !!}}
     </div>
     <hr>
-
- /*
-@forelse ($dataArr as $item)
-<p>Phần tử : {{$item}}</p>
-@empty 
-@endforelse*/
-
-@if($number<0)
-<p>Đây là giá trị hợp lệ</p>
-@elseif($number>=0 && $number<5)
-<p>Số siêu nhỏ</p>
-@elseif($number>=0 && $number<5)
-<p>Số siêu nhỏ</p>
-@elseif($number>=0 && $number<5)
-<p>Số siêu nhỏ</p>
-<p>Số lơn</p>
-@endif
-
-@for($i=1; $i<=10; $i++)
-   <p>Phần tử : {{$i}}</p>
-   @if($i==5)
-   @continue
-    @endif
-@endfor
-
+    @php
+    $message = "Đặt hàng thành công";
+    @endphp
+@include('parts.notice')
+ 
 
 </body>
 
